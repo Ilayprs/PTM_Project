f09c6d1a80b5af2ec2a8810d80638cd9023a7e09
Ilay Preiss <ilay.preiss10@gmail.com>
2024-12-28 14:02:07 +0200

    m

--- EX/test/BinOpAgent.java
+++ EX/test/BinOpAgent.java
@@ -4,6 +4,8 @@ import java.util.function.BinaryOperator;
 
 import test.TopicManagerSingleton.TopicManager;
 
+import static java.lang.Double.isNaN;
+
 public class BinOpAgent implements Agent{
 
     private Node n;
@@ -12,6 +14,9 @@ public class BinOpAgent implements Agent{
     private Topic resultTopic;
     private BinaryOperator<Double> op;
 
+    private Double value1;
+    private Double value2;
+
 
     public BinOpAgent(String name, String topic1, String topic2, String resultTopic, BinaryOperator<Double> op) {
         this.n = new Node(name);
@@ -19,17 +24,21 @@ public class BinOpAgent implements Agent{
         this.topic2 = new Topic(topic2);
         this.resultTopic = new Topic(resultTopic);
         this.op = op;
+
         this.topic1.subscribe(this);
         this.topic2.subscribe(this);
         this.resultTopic.subscribe(this);
-        if(!Double.isNaN(n.getMsg().asDouble)){
+
+        if(!isNaN((n.getMsg().asDouble))){
             this.topic1.publish(this.n.getMsg());
             this.topic2.publish(this.n.getMsg());
         }
-        resultTopic = String.valueOf(op.apply(n.getMsg().asDouble, n.getMsg().asDouble));
-        this.resultTopic.publish(new Message(resultTopic));
 
+        Double result = this.op.apply(this.n.getMsg().asDouble, this.n.getMsg().asDouble);
+        this.resultTopic.publish(new Message(String.valueOf(result)));
 
+        this.value1 = null;
+        this.value2 = null;
     }
 
     @Override
@@ -56,7 +65,20 @@ public class BinOpAgent implements Agent{
     }
     @Override
     public void callback(String topic, Message msg){
-
+        if(msg != null){
+            if(topic1.name.equals(topic)){
+                value1 = (Double) msg.asDouble;
+            }
+            if(topic2.name.equals(topic)){
+                value2 = (Double) msg.asDouble;
+            }
+        }
+        if(!isNaN(value1) && !isNaN(value2)){
+            double result = op.apply(value1, value2);
+            this.resultTopic.publish(new Message(String.valueOf(result)));
+        }
+        value1 = null;
+        value2 = null;
     }
 
 }
