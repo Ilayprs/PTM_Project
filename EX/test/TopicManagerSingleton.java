0311dc4ea0e4ddeb9e601e100e57c9361905cce6
Ilay Preiss <ilay.preiss10@gmail.com>
2024-12-30 14:56:07 +0200

    try

--- EX/test/TopicManagerSingleton.java
+++ EX/test/TopicManagerSingleton.java
@@ -7,18 +7,18 @@ public class TopicManagerSingleton {
 
     public static class TopicManager{
         private static final TopicManager instance = new TopicManager();
-        private Map<String, Topic> topicsMap;//
+        private Map<String, Topic> topicsMap;
         private TopicManager(){
-            topicsMap = new HashMap<>();//
+            topicsMap = new HashMap<>();
         }
-        public Topic getTopic(String name) {//
-            if (topicsMap.containsKey(name)) {//
-                return topicsMap.get(name);//
-            }//
-            Topic newTopic = new Topic(name);//
-            topicsMap.put(name, newTopic);//
-            return newTopic;//
-        }//
+        public Topic getTopic(String name) {
+            if (topicsMap.containsKey(name)) {
+                return topicsMap.get(name);
+            }
+            Topic newTopic = new Topic(name);
+            topicsMap.put(name, newTopic);
+            return newTopic;
+        }
         public void clear(){
             this.topicsMap = new HashMap<>();
         }
