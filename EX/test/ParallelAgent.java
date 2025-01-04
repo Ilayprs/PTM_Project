181e3eb6dd3bb35eaf5f98ec6baa5a84071bb417
Ilay Preiss <ilay.preiss10@gmail.com>
2025-01-04 12:37:57 +0200

    try

---
+++ EX/test/ParallelAgent.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ParallelAgent implements Agent {
+
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
